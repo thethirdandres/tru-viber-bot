15fbfb414c0be0f5a2dd2355b835206be82fe15f
Andres <thethirdandres@gmail.com>
2022-03-01 16:01:24 +0800

    update main menu calltoorder image

--- infrastructure/helpers/composer.js
+++ infrastructure/helpers/composer.js
@@ -33,7 +33,7 @@ module.exports = class Composer {
     static composeMainMenuElements() {
         let mainMenuMsgElement = TemplateBuilder.buildTextMessage('How can we help you today?');
         
-        let asImage = TemplateBuilder.buildImageTemplate("https://storage.googleapis.com/avigate-img-resources/tru-resources/CSP-Hotline.jpg", 6, 4, true, "none");
+        let asImage = TemplateBuilder.buildImageTemplate("https://storage.googleapis.com/avigate-img-resources/tru-resources/CALL%20SHOP%20PLAY%20x%20HOTLINE%20KV.jpg", 6, 4, true, "none");
         let asTitle = TemplateBuilder.buildTitleTemplate("Assisted Shopping");
         let asButtons1 = TemplateBuilder.buildButtonTemplate("Call 0917111TOYS", 6, 1, true, "open-url", "viber://chat?number=%2B639171118697");
         let asButtons2 = TemplateBuilder.buildButtonTemplate("Choose store", 6, 1, false, "reply", "Choose store");
