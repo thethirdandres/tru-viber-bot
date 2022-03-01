f6dd0ee02465b480e7ac0c019cb430a9d74d7794
Andres <thethirdandres@gmail.com>
2022-03-01 14:00:52 +0800

    wrong variable for chatbot_store_name used

--- infrastructure/helpers/factory.js
+++ infrastructure/helpers/factory.js
@@ -8,8 +8,8 @@ module.exports = class Factory {
     static getHandOffDialogue(payload) {
         let payload_tokens = payload.split(" ");
         let contact_number = payload_tokens[1];
-        let business_name_raw = payload_tokens.splice(2).join(" ");
-        let business_name = Helper.lowerCaseAllWordsExceptFirstLetters(business_name_raw);
+        let chatbot_store_name_raw = payload_tokens.splice(2).join(" ");
+        let chatbot_store_name = Helper.lowerCaseAllWordsExceptFirstLetters(chatbot_store_name_raw);
 
         
         let handoffText = TemplateBuilder.buildTextTemplate(`You will be redirected to ${chatbot_store_name}'s Personal Shopper. Continue?`, 6, 6);
