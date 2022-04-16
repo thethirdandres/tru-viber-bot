5a5c9e0f338f201d4ae28689606cd967b160bade
Andres <thethirdandres@gmail.com>
2022-04-16 10:07:46 +0800

    Remove all holy week advisory logic

--- infrastructure/helpers/factory.js
+++ infrastructure/helpers/factory.js
@@ -15,21 +15,7 @@ module.exports = class Factory {
         let handoffText = TemplateBuilder.buildTextTemplate(`You will be redirected to ${chatbot_store_name_full} official chat box, just type "Hello" to start a conversation.`, 6, 6);
         let handoffButton1 = TemplateBuilder.buildButtonTemplate("Confirm", 3, 1, true, "open-url", `viber://chat?number=%2B${contact_number}`);
         let handoffButton2 = TemplateBuilder.buildButtonTemplate("Main Menu", 3, 1, false, "reply", `Main Menu`);
-        
-        const localStoresNotClosedThuFri = ["Antipolo", "Harbor Point, Subic", "Serin Tagaytay", "Solenad 3, Nuvali"];
-        const localStoresClosedFri = ["Ilocos", "Tuguegarao", "Naga", "Lipa", "U.P. Town Center"];
-          
-        // For Holy Week 2022
-        if(localStoresClosedFri.includes(chatbot_store_name)) {
-            handoffText = TemplateBuilder.buildTextTemplate(`Hello! Thank you for reaching out to Toys”R”Us.\n\nIn observance of the Holy Week, we are closed today, April 15 (Good Friday). We will resume regular operating hours on April 16, 2022, Saturday.\n\nThank you and we look forward to serving you!`, 6, 6);
-            handoffButton1 = TemplateBuilder.buildButtonTemplate("Confirm", 3, 1, true, "open-url", `viber://chat?number=%2B${contact_number}`);
-        } else if(!localStoresNotClosedThuFri.includes(chatbot_store_name) && !localStoresClosedFri.includes(chatbot_store_name)) {
-            console.log(!localStoresNotClosedThuFri.includes(chatbot_store_name));
-            console.log(!localStoresClosedFri.includes(chatbot_store_name));
-            handoffText = TemplateBuilder.buildTextTemplate(`Hello! Thank you for reaching out to Toys”R”Us.\n\nIn observance of the Holy Week, we are closed today, April 14 (Maundy Thursday) until April 15 (Good Friday). We will resume regular operating hours on April 16, 2022, Saturday.\n\nThank you and we look forward to serving you!`, 6, 6);
-            handoffButton1 = TemplateBuilder.buildButtonTemplate("Confirm", 3, 1, true, "open-url", `viber://chat?number=%2B${contact_number}`);
-        } 
-
+      
         const cards = [handoffText, handoffButton1, handoffButton2];
         let handOffDialogueBuild = TemplateBuilder.buildJsonTemplate(6, 7, cards);
         let handOffDialogueElement = TemplateBuilder.buildRichMediaMessage(handOffDialogueBuild);
