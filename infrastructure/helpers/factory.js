2c3607176fc6b06e3c0ced29db282545839e3a62
Andres <thethirdandres@gmail.com>
2022-05-24 18:47:51 +0800

    Change handoff message and confirm button

--- infrastructure/helpers/factory.js
+++ infrastructure/helpers/factory.js
@@ -12,9 +12,13 @@ module.exports = class Factory {
         let chatbot_store_name_full = "Toys\"R\"Us ".concat(chatbot_store_name);
         
 
-        let handoffText = TemplateBuilder.buildTextTemplate(`You will be redirected to ${chatbot_store_name_full} official chat box, just type "Hello" to start a conversation.`, 6, 6);
-        let handoffButton1 = TemplateBuilder.buildButtonTemplate("Confirm", 3, 1, true, "open-url", `viber://chat?number=%2B${contact_number}`);
+        // let handoffText = TemplateBuilder.buildTextTemplate(`You will be redirected to ${chatbot_store_name_full} official chat box, just type "Hello" to start a conversation.`, 6, 6);
+        let handoffText = TemplateBuilder.buildTextTemplate(`You will be connected to ${chatbot_store_name_full}'s personal shopper, do you want to continue?`, 6, 6);
+        // let handoffButton1 = TemplateBuilder.buildButtonTemplate("Confirm", 3, 1, true, "open-url", `viber://chat?number=%2B${contact_number}`);
+        let handoffButton1 = TemplateBuilder.buildButtonTemplate("Confirm", 3, 1, true, "reply", `Please wait for a moment as we connect you to our personal shopper.`);
         let handoffButton2 = TemplateBuilder.buildButtonTemplate("Main Menu", 3, 1, false, "reply", `Main Menu`);
+
+        console.log("User being connected to personal shopper.");
       
         const cards = [handoffText, handoffButton1, handoffButton2];
         let handOffDialogueBuild = TemplateBuilder.buildJsonTemplate(6, 7, cards);
