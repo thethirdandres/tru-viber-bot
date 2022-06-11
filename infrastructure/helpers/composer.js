05d2331f2d72094938290488281021ffffc99a46
Andres <thethirdandres@gmail.com>
2022-06-11 14:20:38 +0800

    Change moveTo -> met

--- infrastructure/helpers/composer.js
+++ infrastructure/helpers/composer.js
@@ -42,7 +42,7 @@ module.exports = class Composer {
         let sho = TemplateBuilder.buildImageTitleButtonTemplate("https://storage.googleapis.com/avigate-img-resources/tru-resources/Chatbot%20Pictures/online%20stores/ShopeeMall.jpg", 6, 5, true, "none", "Shopee", "SHOP AT SHOPEE", 6, 1, true, "open-url", "https://shopee.ph/toysrusph");
         let met = TemplateBuilder.buildImageTitleButtonTemplate("https://storage.googleapis.com/avigate-img-resources/general/chatbot-pix_viber-rev-Metromart.jpg", 6, 5, true, "none", "Metromart", "SHOP AT METROMART", 6, 1, true, "open-url", "placeholder.com");
         
-        const cards = [asImage, asButtons1, asButtons2, rob[0], rob[1], rob[2], laz[0], laz[1], laz[2], sho[0], sho[1], sho[2], moveTo[0], moveTo[1], moveTo[2]];
+        const cards = [asImage, asButtons1, asButtons2, rob[0], rob[1], rob[2], laz[0], laz[1], laz[2], sho[0], sho[1], sho[2], met[0], met[1], met[2]];
 
 
         let mainMenuElementBuild = TemplateBuilder.buildJsonTemplate(6, 7, cards);
@@ -60,7 +60,7 @@ module.exports = class Composer {
         const cards = [promosEvents[0], promosEvents[1], promosEvents[2], howShopPlay[0], howShopPlay[1], howShopPlay[2], returnExchange[0], returnExchange[1], returnExchange[2]];
 
 
-        let learnMoreBuild = TemplateBuilder.buildJsonTemplate(6, 7, cards);
+        let learnMoreBuild = TemplateBuilder.buildJsonTemplate(6, 7, cards);  
         let learnMoreElement = TemplateBuilder.buildRichMediaMessage(learnMoreBuild);
 
         return [learnMoreMsgElement, learnMoreElement];
